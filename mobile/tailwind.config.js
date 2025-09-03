b9090b5c267383978fa6e44f71c5db15139c3683
Pema Rinchen <pemarinchen.12.31.2002@gmail.com>
2025-09-03 18:50:01 +0530

    mobile set up

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
